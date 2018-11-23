60881d9edac52ec8eb9e164d9d0fac3da7d175c1
RexTheCapt <cato.jordalen@hotmail.com>
2018-11-23 09:59:49 +0100

    Updated Nuke

--- Assets/Scripts/NukeScript.cs
+++ Assets/Scripts/NukeScript.cs
@@ -8,6 +8,25 @@ namespace Assets.Scripts
 {
     public class NukeScript : MonoBehaviour
     {
+        public float ExpansionSpeed = 5f;
 
+        void Update()
+        {
+            float ExpandBy = Time.deltaTime * ExpansionSpeed;
+            Vector3 expandVector3 = new Vector3(ExpandBy, ExpandBy, ExpandBy);
+
+            transform.localScale += expandVector3;
+
+            if(transform.localScale.z > 100)
+                Destroy(gameObject);
+        }
+
+        void OnTriggerEnter(Collider collider)
+        {
+            if (collider.gameObject.tag == "Enemy")
+            {
+                Destroy(collider.gameObject);
+            }
+        }
     }
 }
